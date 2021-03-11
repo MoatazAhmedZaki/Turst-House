ba0cbf0f7b010206cfe88885e105567f8a83a111
MoatazAhmedZaki <moatazcs@gmailcom>
2021-03-11 19:07:40 +0200

    Updates

---
+++ precache-manifest.fa017fd540c584ea2c0c66f68d4f2775.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/Turst-House/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "06f0a53764748d2a39be",
+    "url": "/Turst-House/static/css/main.95b17f03.chunk.css"
+  },
+  {
+    "revision": "6f8f77fbccd659c8c5e0",
+    "url": "/Turst-House/static/js/runtime~main.8206d75a.js"
+  },
+  {
+    "revision": "1a74c98f94f985c83bd486e26cd31f80",
+    "url": "/Turst-House/static/media/white-logo.1a74c98f.png"
+  },
+  {
+    "revision": "0ddf3d5c08feeb8f6d17",
+    "url": "/Turst-House/static/js/2.31d80eb3.chunk.js"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/Turst-House/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/Turst-House/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/Turst-House/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "06f0a53764748d2a39be",
+    "url": "/Turst-House/static/js/main.af62e88c.chunk.js"
+  },
+  {
+    "revision": "224a830c69c2fa0c2d8b62858361bb2f",
+    "url": "/Turst-House/static/media/uploaded2.224a830c.PNG"
+  },
+  {
+    "revision": "867834305491d827b9efe0cb7ffecf37",
+    "url": "/Turst-House/static/media/about.86783430.jpg"
+  },
+  {
+    "revision": "ca5522d7175ab0fc9d930d930006410e",
+    "url": "/Turst-House/static/media/image2.ca5522d7.jpg"
+  },
+  {
+    "revision": "599558a8d80b2c3750fb98ed060b5372",
+    "url": "/Turst-House/static/media/about-numm.599558a8.jpg"
+  },
+  {
+    "revision": "7667381bc4b2e2bd05565a62e8ccd588",
+    "url": "/Turst-House/static/media/image4.7667381b.jpg"
+  },
+  {
+    "revision": "7aa8dabc26deae3b15546f5a63ffe452",
+    "url": "/Turst-House/static/media/shop.7aa8dabc.jpg"
+  },
+  {
+    "revision": "0ddf3d5c08feeb8f6d17",
+    "url": "/Turst-House/static/css/2.419799cd.chunk.css"
+  },
+  {
+    "revision": "9f9edf3582bbc686a854d6153d53bd3c",
+    "url": "/Turst-House/index.html"
+  }
+];
